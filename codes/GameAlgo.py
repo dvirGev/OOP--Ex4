43ed0fb5b31dfdfdd9ae6ea99b19b306a6fac0c3
dvirBiton1 <dvir199710@gmail.com>
2022-01-02 19:52:29 +0200

    ones was a student

--- codes/GameAlgo.py
+++ codes/GameAlgo.py
@@ -1,37 +1,34 @@
 import json
 from DiGraph import *
-#from GraphAlgo import GraphAlgo
-from classes import *
-
+from GraphAlgo import GraphAlgo
+from codes.classes import pokemon
+import math
 class gameAlgo():
     def __init__(self) -> None:
         self.pokemons = []
         self.agents = []
         self.graph = DiGraph()
-        #self.graphAlgo = GraphAlgo()
+        self.graphAlgo = GraphAlgo()
         
     def update(self, pokemons = None, agents = None, graph = None) ->None:
         if pokemons != None:
-            self.pokemons = []
+            pokemons = pokemons
             pokemons_obj = json.loads(pokemons)
+            pokemons = []
             for poke in pokemons_obj['Pokemons']:
-                self.pokemons.append(pokemon(poke['Pokemon']))
-        
-        if agents != None:
-            self.agents = []
-            agents_obj = json.loads(agents)
-            for age in agents_obj['Agents']:
-                self.agents.append(agent(age['Agent']))
-        
-        if graph != None:
-            self.graph = DiGraph()
-            graph_obj = json.loads(graph)
-            for node in graph_obj["Nodes"]:
-                id = int(node["id"])
-                if "pos" in node:
-                    posData = node["pos"].split(',')
-                    self.graph.add_node(id, (float(posData[0]), float(posData[1]), float(posData[2])))
-                else:
-                    self.graph.add_node(id)
-            for edge in graph_obj["Edges"]:
-                self.graph.add_edge(int(edge["src"]), int(edge["dest"]), float(edge["w"]))
+                pokemons.append(pokemon(poke['Pokemon']))
+
+    def pokemon_src_dest(self, pok: pokemon) -> list:
+        for node1 in self.graph.nodes:
+            for node2 in self.graph.nodes:
+                if self.distanceNodes(self.graph.nodes[node1], self.graph.nodes[node2]) == (self.distancePokNode(self.graph.nodes[node1], pok) + self.distancePokNode(self.graph.nodes[node2], pok)):
+                        return (node1, node2)
+        return None
+
+    def distanceNodes(self, node1: Node, node2: Node) -> float:
+        dis = math.sqrt(pow(node1.location[0] - node2.location[0], 2) + pow(node1.location[1] - node2.location[1], 2))
+        return dis
+
+    def distancePokNode(self, node1: Node, pok: pokemon) -> float:
+        dis = math.sqrt(pow(node1.location[0] - pok.pos[0], 2) + pow(node1.location[1] - pok.pos[1], 2))
+        return dis
