b14b8a972d4f696c7b28799c34f9dda48cdfb358
dvirGev <dvir87879@gmail.com>
2022-01-04 17:48:26 +0200

    i done with the story

--- codes/Ex4.py
+++ codes/Ex4.py
@@ -8,16 +8,18 @@ from GUI import GUI
 from classes import agent
 ###### python codes./Ex4.py
 """sys.argv[1]"""
-subprocess.Popen(['powershell.exe', f'java -jar Ex4_Server_v0.0.jar {14}'])
+subprocess.Popen(['powershell.exe', f'java -jar Ex4_Server_v0.0.jar {sys.argv[1]}'])
 # default port
 PORT = 6666
 # server host (default localhost 127.0.0.1)
 HOST = '127.0.0.1'
 client = Client()
+client.start_connection(HOST, PORT)
+
 game = gameAlgo()
 
 
-client.start_connection(HOST, PORT)
+
 
 def addAgents():
     size = int(json.loads(client.get_info())["GameServer"]["agents"])
@@ -26,7 +28,7 @@ def addAgents():
 addAgents()
 
 game.update(client.get_pokemons(), client.get_agents(), client.get_graph())
-gui = GUI(game)
+gui = GUI(game, client)
 client.start()
 while client.is_running() == 'true':
     game.update(client.get_pokemons(), client.get_agents(), client.get_graph())
--- codes/GUI.py
+++ codes/GUI.py
@@ -1,9 +1,11 @@
+import json
 from os import system
 import pygame
 from pygame import *
 from pygame.time import Clock
 
 from GameAlgo import gameAlgo
+from client import Client
 
 WIDTH, HEIGHT = 1080, 720
 radius = 25
@@ -13,12 +15,15 @@ node = 'GUIData/node.png'
 agent = 'GUIData/agent.gif'
 pikachu = 'GUIData/pikachu.png'
 charmander = 'GUIData/charmander.png'
+squirtle = 'GUIData/squirtle.png'
+bulbasaur = 'GUIData/bulbasaur.png'
 pygame.init()
 mixer.init()
 font.init()
 
 class GUI():
-    def __init__(self, gameAlgo: gameAlgo) -> None:
+    def __init__(self, gameAlgo: gameAlgo, client: Client) -> None:
+        self.client = client
         self.gameAlgo = gameAlgo
         self.screen = display.set_mode((WIDTH, HEIGHT), depth=32, flags=RESIZABLE)
         self.background_image = image.load(background)
@@ -30,10 +35,13 @@ class GUI():
         pikachu_image = pygame.transform.scale(pikachu_image, (35, 35))
         charmander_image = image.load(charmander)
         charmander_image = pygame.transform.scale(charmander_image, (35, 35))
-        self.pokemos_image = [pikachu_image, charmander_image]
+        squirtle_image = image.load(squirtle)
+        squirtle_image = pygame.transform.scale(squirtle_image, (35, 35))
+        bulbasaur_image = image.load(bulbasaur)
+        bulbasaur_image = pygame.transform.scale(bulbasaur_image, (35, 35))
+        self.pokemos_image = [pikachu_image, charmander_image, squirtle_image, bulbasaur_image]
         mixer.music.load(song)
-        mixer.music.play(-1) # If the loops is -1 then the music will repeat indefinitely.
-        mixer.music.play()
+        #mixer.music.play(-1) # If the loops is -1 then the music will repeat indefinitely.
         
         self.min_x = float('inf')
         self.min_y = float('inf')
@@ -47,6 +55,12 @@ class GUI():
             self.min_y = min(self.min_y, y)
             self.max_x = max(self.max_x, x)
             self.max_y = max(self.max_y, y)
+        
+        self.exit_button = Button((28, 172, 74), 2, 2, 70, 20, 'EXIT')
+        self.muzic_button = Button((28, 172, 74), 72, 2, 70, 20, 'MUZIC')
+        self.move_button = Button((28, 172, 74), 142, 2, 70, 20, 'MOVES')
+        self.grade_button = Button((28, 172, 74), 212, 2, 70, 20, 'GRADE')
+        self.isPLay = 0
     def scale(self, data, min_screen, max_screen, min_data, max_data):
         """
         get the scaled data with proportions min_data, max_data
@@ -68,6 +82,18 @@ class GUI():
                 pygame.quit()
                 exit(0)
                 return False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if self.muzic_button.isOver(pygame.mouse.get_pos()):
+                        if self.isPLay % 2:
+                            mixer.music.stop()
+                        else:
+                            mixer.music.play(-1)
+                        self.isPLay+=1
+                    if self.exit_button.isOver(mouse.get_pos()):
+                        pygame.quit()
+                        exit(0)
+                        return False
+        self.drawButtons()
         self.drawEdges()
         self.drawNode()
         self.drawPokemons()
@@ -105,19 +131,57 @@ class GUI():
             # draw the line
             pygame.draw.line(self.screen, Color(61, 72, 126), (src_x, src_y), (dest_x, dest_y), 5)
     def drawPokemons(self):
+        index = 0
         pokemons = self.gameAlgo.pokemons
         for p in pokemons:
             # print(p.__dict__)
             x = self.my_scale(p.pos[0], x=True)
             y = self.my_scale(p.pos[1], y=True)
             
-            self.screen.blit(self.pokemos_image[1], (x, y))
+            self.screen.blit(self.pokemos_image[index%len(self.pokemos_image)], (x, y))
+            index += 1
     def drawAgents(self):
         agents = self.gameAlgo.agents.values()
         for agent in agents:
             x = self.my_scale(agent.pos[0], x=True) - radius/2
             y = self.my_scale(agent.pos[1], y=True) - radius/2  
             
-            self.screen.blit(self.agent_image, (x, y))      
+            self.screen.blit(self.agent_image, (x, y)) 
+    def drawButtons(self)->None:
+        self.muzic_button.draw(self.screen, (0,0,0))
+        self.exit_button.draw(self.screen, (0,0,0)) 
+        self.move_button.text = 'MOVES: ' + str(json.loads(self.client.get_info())["GameServer"]["moves"])
+        self.move_button.draw(self.screen, (0,0,0))
+        self.grade_button.text = 'GRADE: ' + str(json.loads(self.client.get_info())["GameServer"]["grade"])
+        self.grade_button.draw(self.screen, (0,0,0))
+            
+class Button():
+    def __init__(self, color, x,y,width,height, text=''):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+
+    def draw(self,win,outline=None):
+        #Call this method to draw the button on the screen
+        if outline:
+            pygame.draw.rect(win, outline, (self.x-2,self.y-2,self.width+4,self.height+4),0)
+            
+        pygame.draw.rect(win, self.color, (self.x,self.y,self.width,self.height),0)
+        
+        if self.text != '':
+            font = pygame.font.SysFont('comicsans', 10)
+            text = font.render(self.text, 1, (0,0,0))
+            win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
+
+    def isOver(self, pos):
+        #Pos is the mouse position or a tuple of (x,y) coordinates
+        if pos[0] > self.x and pos[0] < self.x + self.width:
+            if pos[1] > self.y and pos[1] < self.y + self.height:
+                return True
+            
+        return False
     
     
